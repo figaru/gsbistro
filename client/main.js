35931e6e2a0af5e22fd434cb1743555dea8ae6f6
Daniel Abrantes <daniel.abrantes95@gmail.com>
2020-06-18 12:03:35 +0100

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,23 @@
+// Client entry point, imports all client code
+
+import '/imports/startup/client';
+import '/imports/startup/both';
+
+
+
+import "/imports/ui/vendor/jquery/dist/jquery.min.js";
+import "/imports/ui/vendor/popper.js/dist/umd/popper.min.js";
+import "/imports/ui/vendor/bootstrap/dist/js/bootstrap.min.js";
+import "/imports/ui/vendor/headroom.js/dist/headroom.min.js";
+
+import "/imports/ui/vendor/onscreen/dist/on-screen.umd.min.js";
+import "/imports/ui/vendor/nouislider/distribute/nouislider.min.js";
+import "/imports/ui/vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js";
+import "/imports/ui/vendor/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js";
+import "/imports/ui/vendor/waypoints/lib/jquery.waypoints.min.js";
+import "/imports/ui/vendor/jarallax/dist/jarallax.min.js";
+import "/imports/ui/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js";
+import "/imports/ui/vendor/vivus/dist/vivus.min.js";
+
+
+import '/imports/ui/scripts/pixel.js';
