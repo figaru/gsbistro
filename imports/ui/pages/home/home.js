9044c7d7ef81fbce41843ddc8202f078630caf92
Daniel Abrantes <daniel.abrantes95@gmail.com>
2020-06-19 10:45:08 +0100

    lang

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -2,3 +2,19 @@ import './home.html';
 
 import '../../components/hello/hello.js';
 import '../../components/info/info.js';
+
+
+Template.App_home.onRendered(function () {
+    /* setTimeout(function(){
+        $("#promo").click();
+    }, 1200); */
+
+    let lang = localStorage.getItem('lang');
+
+    if (lang) {
+        
+    }else{
+
+    }
+
+});
