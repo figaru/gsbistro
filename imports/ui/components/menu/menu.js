8a289588b060a6103d11b5f1b0b0268902f5632e
Daniel Abrantes <daniel.abrantes95@gmail.com>
2020-06-21 20:24:04 +0100

    menu descriptions

--- imports/ui/components/menu/menu.js
+++ imports/ui/components/menu/menu.js
@@ -1,1 +1,28 @@
 import './menu.html';
+
+Template.menu.onCreated(function () {
+    this.lang = new ReactiveVar();
+});
+Template.menu.onRendered(function () {
+    let lang = localStorage.getItem('lang');
+    if (!lang || typeof lang == "undefined" || lang == "undefined") {
+        localStorage.setItem("lang", "pt");
+        this.lang.set("pt");
+    } else {
+        this.lang.set(lang);
+    }
+});
+
+
+Template.menu.helpers({
+    'lang': function(){
+        let lang = Template.instance().lang.get();
+
+        return lang;
+    },  
+    'get_lang': function (check) {
+        let lang = Template.instance().lang.get();
+
+        return check == lang ? true : false;
+    }
+});
